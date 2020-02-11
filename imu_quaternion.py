8a9afb4c6af734c2eb0d5777c86d8e7e2d8787f8
Kyle Coelho <coelho.k@husky.neu.edu>
2020-02-11 11:57:50 -0500

    Updated Plotting

--- imu_quaternion.py
+++ imu_quaternion.py
@@ -37,30 +37,32 @@ plt.show()"""
 
 
 fig = plt.figure()
-ax = plt.axes(projection='3d', aspect = 'auto', autoscale_on = True)
+ax = plt.axes(projection='3d')
 plt.ion()
 
 shoulder = [0, 0, 2]
-updated_point = [0, 0, 1]
-test = [0, 0, 1]
+updated_point = [0, 0, 0.1]
+test = [0, 0, 0.2]
 # Calculate the rotation matrix based on the updated vector ??
 # Remove the infinte lines
 
 def makeFig():
     #ax.plot([1.5,cnt], [1.5,cnt], [2,cnt])
     #lines = ax.plot([0,test[0]], [0,test[1]], [2,test[2]], marker = '.', color = 'blue')
-    ax.scatter([0,test[0]], [0,test[1]], [0,test[2]], color = 'blue')
+    ax.plot([0,test[0]], [0,test[1]], [2,test[2]], color = 'blue')
     #ax.plot(updated_point, [1.5, 1.5, 2])
 
 while True:
     data = imu.readline().decode('ascii').replace('\r\n', '').replace('\t\tCALIBRATION', '').replace(':', '').replace('\t\t', '')
     x = data.split(' ')
-    #print(x)
+    print(x)
     if x[0] == 'qW':
         quaternions = quaternions.append({'qw':float(x[1]), 'qx':float(x[3]), 'qy':float(x[5]), 'qz':float(x[7])}, ignore_index=True)
         q = quaternions.loc[cnt]
+        #q_norm = transforms3d.quaternions.qnorm(q)
+        #q = [q[0]/q_norm, q[1]/q_norm, q[2]/q_norm, q[3]/q_norm]
         q_conj = [q[0], -q[1], -q[2], -q[3]]
-        print(q)
+        #print(q)
         print(np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2))   # Magnitude
         #M = transforms3d.quaternions.quat2mat(quaternions.loc[cnt])
         # - Addition method -- not sure how it works
@@ -73,14 +75,27 @@ while True:
 
         #-----WORKS WELL ???? with always computing on the same point(elbow)-----#
         test = transforms3d.quaternions.rotate_vector(updated_point, q)
-        #------------------------------------------------------------------------
-        #test = np.matmul(np.matmul(q, test), q_conj)
+        #------------------------------------------------------------------------#
+        # Normalize  --> Divide each component by the magnitude
+        # Trying to keep the segment as 0.2 magnitude
 
+        test_mag = np.sqrt(test[0]**2 + test[1]**2 + test[2]**2)
+        print(test_mag)
+        test = [test[0] * 0.5/test_mag, test[1] * 0.5/test_mag, test[2] * 0.5/test_mag]
 
         print(test)
-        makeFig() 
+        # makeFig()
+        ax.plot([0, 0], [0, 0], [1, 0.5], color = 'blue', marker = '.') 
+        ax.plot([0, 0.2], [0, 0.2], [0.5, 0], color = 'blue', marker = '.') 
+        ax.plot([0, -0.2], [0, -0.2], [0.5, 0], color = 'blue', marker = '.') 
+        ax.plot([0, test[0]], [0, test[1]], [0.75, test[2]], color = 'blue', marker = '.')
+        #ax.plot([test[0], 0.2], [test[1], 0.2], [test[2], 0.2], color = 'blue', marker = '.')
+        plt.autoscale(False)
+        plt.xlim(-1,1)
+        plt.ylim(-1.1)
+        
         plt.pause(0.0001)
-
+        ax.cla()
         cnt += 1
 
         #if np.mod(cnt, 100) == 0:
