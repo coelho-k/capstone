d74300b4688504a2a84616911a1d2ee892dc82cf
Kyle Coelho <coelho.k@husky.neu.edu>
2020-02-17 18:24:56 -0500

    Changing axes by 90 y-rotation

--- imu_quaternion.py
+++ imu_quaternion.py
@@ -10,6 +10,13 @@ import transforms3d
 
 # -------------------------------
 # Normalize the quaternion data before sending it !!?? --> Done in Arduino Code
+
+        # Normalize  --> Divide each component by the magnitude
+        # Trying to keep the segment as 0.2 magnitude
+
+        #test_mag = np.sqrt(test[0]**2 + test[1]**2 + (test[2] - 0.5)**2)
+        #print('Test mag = ', test_mag)
+        #test = [test[0] / test_mag, test[1] / test_mag, test[2] / test_mag]
 # -------------------------------
 
 imu = serial.Serial('/dev/ttyACM0', 115200)
@@ -25,10 +32,11 @@ plt.ion()
 
 
 
-# SCALE BASED ON HEIGHT INPUT
+# SCALE BASED ON HEIGHT INPUT --> TO DO
 shoulder = np.array([0, 0, 2])
 updated_point = np.array([0, 0, 1])
 test = np.array([0, 0, 1])
+axis_change = np.array([0.7071, 0, 0.7071, 0])
 #q_rest = [0.3839, -0.4862, 0.4019, 0.6743] --> For trying to create a reference rotation
 
 """
@@ -49,27 +57,23 @@ while True:
         quaternions = quaternions.append({'qw':float(x[1]), 'qx':float(x[3]), 'qy':float(x[5]), 'qz':float(x[7])}, ignore_index=True)
         q = quaternions.loc[cnt]
 
-        # Trying to shift the axes
-        #temp = q[1]
-        #q[2] = -temp
-        #q[1] = q[2]
+        # ------------------- Print Checks ----------------------------------
         print(q)
+        print(np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2))   
+        # -------------------------------------------------------------------
 
-        print(np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2))   # Magnitude
-
-        # Implementing the rotation about a fixed point (shoulder)
+        # ----------------- Implementing the rotation about a fixed point (shoulder) ---------------------
         temp = updated_point - shoulder
         test = transforms3d.quaternions.rotate_vector(temp, q)
         test = shoulder + test
-        #------------------------------------------------------------------------#
-        # Normalize  --> Divide each component by the magnitude
-        # Trying to keep the segment as 0.2 magnitude
-
-        #test_mag = np.sqrt(test[0]**2 + test[1]**2 + (test[2] - 0.5)**2)
-        #print('Test mag = ', test_mag)
-        #test = [test[0] / test_mag, test[1] / test_mag, test[2] / test_mag]
-
+        # -------------------------------------------------------------------------------------------------#
+        # -------------------- Aligning the IMU axes with shoulder ----------------------------------------#
+        temp = test - shoulder
+        test = transforms3d.quaternions.rotate_vector(temp, axis_change)
+        test = shoulder + test
         print('New Co-ordinates = ', test)
+        # -------------------------------------------------------------------------------------------------#
+
         # makeFig()
         ax.plot([0, 0], [0, 0], [2, 1], color = 'blue', marker = '.')   # TORSE
         ax.plot([0, 0.2], [0, 0.2], [1, 0], color = 'blue', marker = '.')   # LEG
