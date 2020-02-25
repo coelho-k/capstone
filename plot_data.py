d28daf20603954183206d44f88cc661da623251a
Kyle Coelho <coelho.k@husky.neu.edu>
2020-02-25 17:54:37 -0500

    Updated

--- plot_data.py
+++ plot_data.py
@@ -1,61 +1,15 @@
-import matplotlib.pyplot as plot 
+import matplotlib.pyplot as plt 
 import numpy as np 
 import pandas as pd 
 
-data = pd.read_csv(r'up_down.csv')
+data = pd.read_csv(r'abduction_25_2_20.csv')
+mag = []
 
-#plt.plot(data.loc[:, '1.5332010933747549'] - np.mean(data.loc[:, '1.5332010933747549']))
+for ii in range(len(data.loc[:, 'x'])):
+    mag.append(np.sqrt(data.loc[ii, 'x']**2 + data.loc[ii, 'y']**2 + data.loc[ii, 'z']**2))
+
+#plt.plot(data.loc[:, 'x'] - np.mean(data.loc[:, 'y']))
 #plt.show()
 
-time        = np.arange(0, 10, 0.1)
-
- 
-
-# Amplitude of the sine wave is sine of a variable like time
-
-amplitude   = np.sin(time)
-
- 
-
-# Plot a sine wave using time and amplitude obtained for the sine wave
-
-plot.plot(time, amplitude)
-
- 
-
-# Give a title for the sine wave plot
-
-plot.title('Sine wave')
-
- 
-
-# Give x axis label for the sine wave plot
-
-plot.xlabel('Time')
-
- 
-
-# Give y axis label for the sine wave plot
-
-plot.ylabel('Amplitude = sin(time)')
-
- 
-
-plot.grid(True, which='both')
-
- 
-
-plot.axhline(y=0, color='k')
-
-plot.hlines(0.5, 0, 10, color = 'red')
-plot.hlines(-0.5, 0, 10, color= 'red')
-
-plot.plot(time, amplitude * np.exp(-time * 0.5))
-
-plot.show()
-
- 
-
-# Display the sine wave
-
-plot.show()
+plt.plot(mag)
+plt.show()
